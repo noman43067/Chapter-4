6d8406390dcdb56bb37593535a6ece6df592bbbe
Anmol Noman <noman43067@gmail.com>
2024-08-13 01:13:46 -0700

    Add files via upload

---
+++ task today/script.js
@@ -0,0 +1,15 @@
+var contain = '$ and _ . For example : $my_1st Variables';
+document.write("Variable names can only contain , numbers ,"+" "+ contain + "<br/>");
+console.log(contain);
+
+var letter = '$ or _ . For example : $name, _name or name';
+document.write("Variable must begin with a letter" +" "+ letter + "<br/>");
+console.log(letter);
+
+var names = 'Variable names are case sensitive';
+document.write(names + "<br/>");
+console.log(names);
+
+var jsKeyword = "Variables names should not be JS Keywords";
+document.write(jsKeyword);
+console.log(jsKeyword);
